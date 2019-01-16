909c8da449dfdfbe51b1b7a9f343aeeccc3e5660
NilayRai <40742612+NilayRai@users.noreply.github.com>
2019-01-17 01:33:16 +0530

    Update DuplicateElement.java

--- src/main/java/com/duplicate/demo/DuplicateElement.java
+++ src/main/java/com/duplicate/demo/DuplicateElement.java
@@ -3,5 +3,6 @@ package com.duplicate.demo;
 public class DuplicateElement {
 	public static void main(String[] args) {
 		System.out.println("Hello Java");
+		System.out.println("Hello India Java");
 	}
 }
